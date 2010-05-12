c8a6d6681c00021b8d44e4cd4a6b06175af25639
kduncan <kduncan@emotivellc.com>
2010-05-12 15:26:11 -0700

    Edited file to retrieve first name, last name and email from facebook and add to your mysql database.

--- index.php
+++ index.php
@@ -10,17 +10,21 @@ $facebook = new Facebook($api_key, $secret);
 $facebook->require_frame();
 $user = $facebook->require_login();
 
-$user_details = $facebook->api_client->users_getInfo($user, "email");
+$user_details = $facebook->api_client->users_getInfo($user, "first_name, last_name, email");
 
 if (!is_array($user_details)) {
  $data["email"] = '';
+ $data["first_name"] = '';
+ $data["last_name"] = '';
 }
  else {
- $emails = $data["email"] = $user_details[0]["email"];
+$emails = $data["email"] = $user_details[0]["email"];
+$first_name = $data["first_name"] = $user_details[0]["first_name"];
+$last_name = $data["last_name"] = $user_details[0]["last_name"];
 }
 
 $mysqli = mysqli_connect ("localhost", "username", "password", "database");
-$sql = "INSERT INTO emails (email_address) VALUES ('$emails')";
+$sql = "INSERT INTO emails (email_address, first_name, last_name) VALUES ('$emails', '$first_name', '$last_name')";
 $res = mysqli_query($mysqli, $sql);
 
 mysqli_close($mysqli);
